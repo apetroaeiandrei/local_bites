c1cfa38b873f27c0905bbf6b791d02db379d3e26
Andrei Apetroaei <andrei.apetroaei@gmail.com>
2022-12-06 18:32:29 +0200

    Removed useless image_picker lib

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -12,7 +12,6 @@ import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:firebase_storage_web/firebase_storage_web.dart';
 import 'package:flutter_keyboard_visibility_web/flutter_keyboard_visibility_web.dart';
-import 'package:image_picker_for_web/image_picker_for_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 
@@ -26,7 +25,6 @@ void registerPlugins(Registrar registrar) {
   FirebaseCoreWeb.registerWith(registrar);
   FirebaseStorageWeb.registerWith(registrar);
   FlutterKeyboardVisibilityPlugin.registerWith(registrar);
-  ImagePickerPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
