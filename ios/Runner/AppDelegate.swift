363a9924eb780a2990b4352ff63e14879ee51dde
Andrei Apetroaei <andrei.apetroaei@gmail.com>
2022-12-16 20:50:14 +0200

    Added ios configuration for prod

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -5,18 +5,28 @@ import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-  //Configure AppCheck for iOS debug builds
-  #if DEBUG
-      let providerFactory = AppCheckDebugProviderFactory()
-      AppCheck.setAppCheckProviderFactory(providerFactory)
-  #endif
-
-    GMSServices.provideAPIKey("YOUR_MAPS_API_KEY")
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        
+        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let flavorChannel = FlutterMethodChannel(name: "flavor",
+                                                 binaryMessenger: controller.binaryMessenger)
+        flavorChannel.setMethodCallHandler({
+            (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+            let flavor = Bundle.main.infoDictionary?["Flavor"] as? String
+            result(flavor)
+        })
+        
+        //Configure AppCheck for iOS debug builds
+#if DEBUG
+        let providerFactory = AppCheckDebugProviderFactory()
+        AppCheck.setAppCheckProviderFactory(providerFactory)
+#endif
+        
+        GMSServices.provideAPIKey("YOUR_MAPS_API_KEY")
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
