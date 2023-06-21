c8e98ba598c4adee00f0dc56da15af208cdb3e21
Andrei Apetroaei <andrei.apetroaei@gmail.com>
2023-06-21 18:57:32 +0300

    implemented flutter_local_notifications
    Register Android notifications channel for vouchers

--- lib/repos/notifications_repo.dart
+++ lib/repos/notifications_repo.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:local/repos/user_repo.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -11,8 +12,13 @@ class NotificationsRepo {
   static const String notificationTopic = 'topic_';
   static const String topicPromo = 'promo';
   static const String sharedPrefsTokenTimestamp = 'last_token_timestamp';
+  static const String vouchersChannelId = 'vouchers_notifications';
+  static const String vouchersChannelName = 'Vouchere';
+
   static NotificationsRepo? _instance;
   final UserRepo _userRepo;
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
   NotificationsRepo._privateConstructor(this._userRepo);
 
@@ -20,6 +26,13 @@ class NotificationsRepo {
     _instance ??= NotificationsRepo._privateConstructor(userRepo);
     return _instance!;
   }
+
+  static const AndroidNotificationChannel pushVouchersChannel =
+      AndroidNotificationChannel(
+    vouchersChannelId,
+    vouchersChannelName,
+    importance: Importance.defaultImportance,
+  );
 
   Future<bool> registerNotifications() async {
     FirebaseMessaging messaging = FirebaseMessaging.instance;
@@ -52,7 +65,25 @@ class NotificationsRepo {
     if (!prefs.containsKey(notificationTopic + topicPromo)) {
       await subscribeToTopic(topicPromo);
     }
+
+    _initLocalNotificationsPlugin();
     return settings.authorizationStatus == AuthorizationStatus.authorized;
+  }
+
+  _initLocalNotificationsPlugin() async {
+    var initializationSettingsAndroid =
+        const AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettingsIOS = const DarwinInitializationSettings();
+    var initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+    flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+    );
+
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(pushVouchersChannel);
   }
 
   updateFcmToken() async {
